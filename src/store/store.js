1255c4432a6176d31bb3255f7a0768de33895b1e
David Berri <dwbwill@gmail.com>
2018-04-24 13:06:30 -0300

    Imported actions.js

--- src/store/store.js
+++ src/store/store.js
@@ -4,9 +4,12 @@ import VueX from 'vuex'
 import stocks from './modules/stocks'
 import portfolio from './modules/portfolio'
 
+import * as actions from './actions'
+
 Vue.use(VueX);
 
 export default new VueX.Store({
+	actions,
 	modules: {
 		stocks,
 		portfolio
