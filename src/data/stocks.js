77ffe7347636c8c000531ad71d5f9b06e18a2fb4
David Berri <dwbwill@gmail.com>
2018-04-23 21:57:03 -0300

    Added dummy stocks file

---
+++ src/data/stocks.js
@@ -0,0 +1,26 @@
+export default [
+	{
+		'id': 1,
+		'name': 'BMW',
+		'price': 85,
+		'quantity': 1250
+	},
+	{
+		'id': 2,
+		'name': 'Apple',
+		'price': 100,
+		'quantity': 960
+	},
+	{
+		'id': 3,
+		'name': 'Google',
+		'price': 100,
+		'quantity': 743
+	},
+	{
+		'id': 4,
+		'name': 'Tesla',
+		'price': 52,
+		'quantity': 432
+	}
+]
