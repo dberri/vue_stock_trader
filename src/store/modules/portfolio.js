9264fa3b79860158821db3653b2d43babedc88e9
David Berri <dwbwill@gmail.com>
2018-04-24 12:58:59 -0300

    New action: SET_PORTFOLIO

--- src/store/modules/portfolio.js
+++ src/store/modules/portfolio.js
@@ -25,6 +25,10 @@ const mutations = {
 		}
 		state.funds += stockPrice * quantity;
 	}
+	'SET_PORTFOLIO' (state, {portfolio}) {
+		state.funds = portfolio.funds;
+		state.stocks = portfolio.stockPortfolio ? portfolio.stockPortfolio : []
+	}
 };
 
 const actions = {
