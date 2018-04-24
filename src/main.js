59e855d97d4eff720b4baa2a387b2f5d0772670d
David Berri <dwbwill@gmail.com>
2018-04-24 12:59:31 -0300

    Add vue-resource to the project and set the root to a firebase project

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 import App from './App.vue'
 
 import { routes } from './routes'
 import store from './store/store';
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://vuejs-stock-trader-69525.firebaseio.com/';
+
+Vue.filter('currency', (value) => {
+	return '$ ' + value.toLocaleString();
+})
 
 const router = new VueRouter({ 
   routes: routes,
